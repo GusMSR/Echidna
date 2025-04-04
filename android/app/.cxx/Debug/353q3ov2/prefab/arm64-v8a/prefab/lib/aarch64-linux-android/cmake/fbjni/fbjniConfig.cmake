20b5c5101d3075ad08932e0506829e70141f87ae
GusSedano <53sedanoruvalcabagustavo3a@gmail.com>
2025-04-03 21:46:28 -0600

    Evaluation fully working on web, working 95% on android; need to change to a static server, potentially making it easier for both versions

---
+++ android/app/.cxx/Debug/353q3ov2/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/fbjni/fbjniConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET fbjni::fbjni)
+add_library(fbjni::fbjni SHARED IMPORTED)
+set_target_properties(fbjni::fbjni PROPERTIES
+    IMPORTED_LOCATION "C:/Users/Gusta/.gradle/caches/8.10.2/transforms/a14fb93a6ff815d3dc43a1cc38e5a120/transformed/fbjni-0.6.0/prefab/modules/fbjni/libs/android.arm64-v8a/libfbjni.so"
+    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/Gusta/.gradle/caches/8.10.2/transforms/a14fb93a6ff815d3dc43a1cc38e5a120/transformed/fbjni-0.6.0/prefab/modules/fbjni/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
