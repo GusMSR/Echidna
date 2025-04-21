a5e50f6d63354ddb783a68f7e4edb6b2a88fd796
GusSedano <53sedanoruvalcabagustavo3a@gmail.com>
2025-04-21 12:26:47 -0600

    communication working in webview

--- app/Analysis.tsx
+++ app/Analysis.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { View, Button, StyleSheet, Platform } from "react-native";
 import { WebView } from "react-native-webview";
 
@@ -12,17 +12,23 @@ import { EvaluatedPosition, Report, analyse, EngineLine } from '../src/Utils/che
 
 Amplify.configure(amplifyconfig);
 
-declare global {
+declare global { //Functions for interaction with iframe
   interface Window {
     parentCallback: ( rawEvals : any[]) => void;
-    childCallback: (  fenPositions: String[] ) => void;
+    childCallback: ( fenPositions: String[]) => void;
   }
 }
 
-
 export default function AnalysisScreen() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [analysisStarted, setAnalysisStarted] = useState(false);
+  const [analysisStartedWeb, setAnalysisStartedWeb] = useState(false);
+  const [analysisStartedMov, setAnalysisStartedMov] = useState(false);
+  const webViewRef = useRef<WebView>(null);
+
+  const pgnContent = "1. e4 d5 2. exd5 Qxd5 3. Nc3 Qa5 4. Nf3 Bg4 5. Be2 Nc6 6. O-O O-O-O 7. d3 e6 8. Be3 Nf6 9. h3 Bh5 10. Ng5 Bg6 11. Nf3 h6 12. Qd2 e5 13. b4 Nxb4 14. a3 Nc6 15. Rab1 e4 16. Nb5 exf3 17. Nxa7+ Nxa7 18. Qxa5 fxe2 19. Rfe1 Nc6 20. Qb5 Bxd3 1-0";
+  let parsedPGN: ParseResult | null = null;
+  let positions: String [] = [];
+  let evaluatedPositions: EvaluatedPosition[] = [];
 
   async function currentAuthenticatedUser() {
     try {
@@ -31,7 +37,7 @@ export default function AnalysisScreen() {
       console.log(`Username: ${username}, UserID: ${userId}`);
     } catch (err) {
       console.log(err);
-      // router.replace('/SignIn'); // Uncomment if you want redirection
+      // router.replace('/SignIn'); // Redirect
     }
   }
 
@@ -39,12 +45,42 @@ export default function AnalysisScreen() {
     currentAuthenticatedUser();
   }, []);
 
-  const handleStartAnalysis = async () => {
-    const pgnContent = "1. e4 d5 2. exd5 Qxd5 3. Nc3 Qa5 4. Nf3 Bg4 5. Be2 Nc6 6. O-O O-O-O 7. d3 e6 8. Be3 Nf6 9. h3 Bh5 10. Ng5 Bg6 11. Nf3 h6 12. Qd2 e5 13. b4 Nxb4 14. a3 Nc6 15. Rab1 e4 16. Nb5 exf3 17. Nxa7+ Nxa7 18. Qxa5 fxe2 19. Rfe1 Nc6 20. Qb5 Bxd3 1-0";
-    let parsedPGN: ParseResult | null = null;
-    let positions: String [] = [];
-    let evaluatedPositions: EvaluatedPosition[] = [];
+  //Expose function to webview, that's why is outside handleStartAnalysis
+    const handleMessage = async (event: any) => {
+      // Data from WebView is in event.nativeEvent.data
+      const data = event.nativeEvent.data;
+      console.log('Received from WebView');
+      //Parse data 
+      try {
+          const parsedData = JSON.parse(data);
+          console.log('Parsed Data');
+          const evaluatedPositions2: EvaluatedPosition[] = parsedData.map((raw: any) => ({
+            move: { san: "", uci: "" }, // You can inject real move data if needed
+            fen: raw.fen,
+            topLines: raw.topLines.map((line: any) => ({
+              id: line.id,
+              depth: line.depth,
+              evaluation: {
+                type: line.evaluation.type,
+                value: line.evaluation.value,
+              },
+              moveUCI: line.moveUCI,
+            })),
+            worker: raw.worker,
+          }));
+          evaluatedPositions = evaluatedPositions2;      
+          console.log("Before evaluating:");
+          console.log(evaluatedPositions);
+          let results: Report = await analyse(evaluatedPositions);
+          console.log("Results:");
+          console.log(results);
+          setAnalysisStartedMov(false);
+      } catch (e) {
+          console.log('Error:', e)
+      }
+    };
 
+  const handleStartAnalysisWeb = async () => {
     // Call the method through iframe window
     const iframe = document.getElementById("iframe") as HTMLIFrameElement;
     function waitForEvaluation(): Promise<EvaluatedPosition[]> {
@@ -72,10 +108,10 @@ export default function AnalysisScreen() {
     if (pgnContent) {
       parsedPGN = parsePGN(pgnContent);
       console.log("Parsed PGN");
-      setAnalysisStarted(true);
+      setAnalysisStartedWeb(true);
       positions = parsedPGN!.positions.map(pos => pos.fen);
     } else {
-      setAnalysisStarted(false);
+      setAnalysisStartedWeb(false);
       console.error("Failed to load PGN content.");
       return;
     }
@@ -97,10 +133,43 @@ export default function AnalysisScreen() {
       let results: Report = await analyse(evaluatedPositions);
       console.log("Results:");
       console.log(results);
-      setAnalysisStarted(false);
+      setAnalysisStartedWeb(false);
     } catch (error) {
       console.error("Error during analysis:", error);
-      setAnalysisStarted(false);
+      setAnalysisStartedWeb(false);
+    }
+  };
+
+  const handleStartAnalysisMov = async () => {
+    // Call the method through webview
+    const sendDataToWebView = ( fenArray: String[]) => {
+      const data = { message: fenArray};
+      webViewRef.current!.postMessage(JSON.stringify(data));
+    };
+
+    if (pgnContent) {
+      parsedPGN = parsePGN(pgnContent);
+      console.log("Parsed PGN mobile");
+      setAnalysisStartedMov(true);
+      positions = parsedPGN!.positions.map(pos => pos.fen);
+    } else {
+      setAnalysisStartedMov(false);
+      console.error("Failed to load PGN content.");
+      return;
+    }
+
+    try {
+      evaluatedPositions = evaluatedPositions.map((evalPos, i) => {
+        if (i === 0) return evalPos; // skip the starting position
+        return {
+          ...evalPos,
+          move: parsedPGN!.positions[i]?.move!
+        };
+      });  
+      sendDataToWebView(positions);
+    } catch (error) {
+      console.error("Error during analysis:", error);
+      setAnalysisStartedMov(false);
     }
   };
 
@@ -111,9 +180,9 @@ export default function AnalysisScreen() {
       <View style={styles.container}>
       <Chessboard boardSize={400} />
 
-      <Button title={analysisStarted ? "Analyzing..." : "Start Game Analysis"} 
-        onPress={handleStartAnalysis} 
-        disabled={analysisStarted} 
+      <Button title={analysisStartedWeb ? "Analyzing..." : "Start Game Analysis"} 
+        onPress={handleStartAnalysisWeb} 
+        disabled={analysisStartedWeb} 
       />
       <iframe
         id='iframe'
@@ -129,17 +198,18 @@ export default function AnalysisScreen() {
     <View style={styles.container}>
       <Chessboard boardSize={400} />
 
-      <Button title={analysisStarted ? "Analyzing..." : "Start Game Analysis"} 
-        onPress={handleStartAnalysis} 
-        disabled={analysisStarted} 
+      <Button title={analysisStartedMov ? "Analyzing..." : "Start Game Analysis"} 
+        onPress={handleStartAnalysisMov} 
+        disabled={analysisStartedMov} 
       />
       
       <WebView
+        ref={webViewRef}
         originWhitelist={["*"]}
         allowFileAccess={true}
         allowUniversalAccessFromFileURLs={true}
         onLoad={() => {}}
-        onMessage={(event) => {}}
+        onMessage={handleMessage}
 	      javaScriptEnabled={true}
         domStorageEnabled={true}
 
